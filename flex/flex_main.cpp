2ecbffed7e659612f2a19963f936758b387cd85c
Arun Bhati <arnbhati@gmail.com>
2014-03-23 15:41:52 +0530

    lex file updated

--- flex/flex_main.cpp
+++ flex/flex_main.cpp
@@ -13,25 +13,14 @@ int main() {
     cout<<file_name;
     freopen(file_name.c_str(), "r", stdin);
     string s;
+    static map<int,string> keyWord = {{0, "eoi"},{1, "auto"},{2, "break"},{3, "case"},{4, "char"},{5, "const"},{6, "continue"},{7, "default"},{8, "do"},{9, "double"},{10, "else"},{11, "enum"},{12, "extern"},{13, "float"},{14, "for"},{15, "goto"},{16, "if"},{17, "int"},{18, "long"},{19, "register"},{20, "return"},{21, "short"},{22, "signed"},{23, "sizeof"},{24, "static"},{25, "struct"},{26, "switch"},{27, "typedef"},{28, "union"},{29, "unsigned"},{30, "void"},{31, "volatile"},{32, "while"},{33, "main"},{40, "plus"},{41, "minus"},{42, "times"},{43, "div"},{44, "modulo"},{45, "equals"},{46, "pluseq"},{47, "minuseq"},{48, "timeseq"},{49, "diveq"},{50, "moduloeq"},{51, "empeq"},{52, "oreq"},{53, "xoreq"},{54, "lshifteq"},{55, "rshifteq"},{56, "bitand"},{57, "bitor"},{58, "bitxor"},{59, "lshift"},{60, "rshift"},{61, "not"},{62, "and"},{63, "or"},{64, "requal"},{65, "notequal"},{66, "incr"},{67, "decr"},{68, "less"},{69, "lesseq"},{70, "greater"},{71, "greatereq"},{80, "hash"},{81, "dollar"},{82, "atrate"},{83, "ls"},{84, "rs"},{85, "lb"},{86, "rb"},{87, "lp"},{88, "rp"},{89, "qmark"},{90, "colon"},{91, "dot"},{92, "ptrref"},{93, "semi"},{94, "comma"},{95, "number"},{96, "number"},{97, "number"},{98, "number"},{99, "identifier"},{100, "identifier"},{101, "identifier"},};
+
     int d = 1;
     while(d) {
         d = yylex();
         s.assign(yytext, yytext + yyleng);
-        if(d>=1 && d<=33)
-            cout<<"< "<<s<<" , keyword >\n";
-        else if(d>=40 && d<=71)
-            cout<<"< "<<s<<" , operator >\n";
-        else if(d>=80 && d<=94)
-            cout<<"< "<<s<<" , symbol >\n";
-        else if(d>=95 && d<=98)
-            cout<<"< "<<s<<" , constant >\n";
-        else if(d==99)
-            cout<<"< "<<s<<" , identifier >\n";
-        else if(d==100)
-            cout<<"< "<<s<<" , string >\n";
-        else if(d==101)
-            cout<<"< "<<s<<" , character >\n";
-    }
+		cout << keyWord[d] << endl ;
+   }
     fclose(stdin);
     return 0;
 }
